421cab30a144d20e8525356134ebeb7db4e3a474
Florencia A. Ríos <floor.riios@gmail.com>
2026-04-09 20:41:22 -0300

    Create ConjuntoTDA.java

---
+++ Bloque 3/Ejercicio 4/ConjuntoTDA.java
@@ -0,0 +1,8 @@
+public interface ConjuntoTDA {
+    void InicializarConjunto();
+    void Agregar(String x);
+    void Sacar(String x);
+    boolean ConjuntoVacio();
+    boolean Pertenece(String x);
+    String Elegir();
+}
