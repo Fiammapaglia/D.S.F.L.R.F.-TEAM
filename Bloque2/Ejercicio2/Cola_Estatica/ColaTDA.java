b0851afc9c3d2e0d863052ae78086806739504a4
Florencia A. Ríos <floor.riios@gmail.com>
2026-04-09 16:08:34 -0300

    Create ColaTDA.java

---
+++ Bloque2/Ejercicio2/Cola_Estatica/ColaTDA.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public interface ColaTDA {
+    void InicializarCola();
+
+    void Acolar(int var1);
+
+    void Desacolar();
+
+    int Primero();
+
+    boolean ColaVacia();
+}
